1c42c2fb28fff12c90e1406ba27374193a70347a
Bruno Heinrich <brunu1994@gmail.com>
2021-06-24 23:08:35 -0300

    Reseting App.js

--- App.js
+++ App.js
@@ -1,62 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState} from 'react';
-import { FlatList, StyleSheet, Text, View, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import Header from './components/header';
-import TodoItem from './components/todoItem';
-import AddTodo from './components/addTodo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    {text: 'buy coffe', key: '1'},
-    {text: 'create an app', key: '2'},
-    {text: 'play on the switch', key: '3'},
-  ]);
-
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key != key);
-    })
-  }
-
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setTodos((prevTodos) => {
-        return [
-          {text: text, key: Math.random().toString()},
-          ...prevTodos
-        ];
-      })
-    } else {
-      Alert.alert('OOPS', 'Todos must be over 3 chars long', [
-        {
-          text: 'OK', onPress: () => console.log("Fechou o alert")
-        }
-      ])
-    }
-  }
-
   return (
-    <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-      console.log("Dismiss keyboard");
-    }}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler}/>
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({item}) => (
-                // <Text>{item.text}</Text>
-                <TodoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
 
@@ -64,11 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  content: {
-    padding: 40,
-  },
-  list: {
-    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
---
+++ AppToDo.js
@@ -0,0 +1,74 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState} from 'react';
+import { FlatList, StyleSheet, Text, View, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'buy coffe', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'play on the switch', key: '3'},
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          {text: text, key: Math.random().toString()},
+          ...prevTodos
+        ];
+      })
+    } else {
+      Alert.alert('OOPS', 'Todos must be over 3 chars long', [
+        {
+          text: 'OK', onPress: () => console.log("Fechou o alert")
+        }
+      ])
+    }
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log("Dismiss keyboard");
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({item}) => (
+                // <Text>{item.text}</Text>
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
