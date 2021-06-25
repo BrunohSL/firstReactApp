22c71cf588c5625c58a2c81f546c8452896ae90f
Bruno Heinrich <brunu1994@gmail.com>
2021-06-25 15:41:21 -0300

    Sending params to the review page

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function ReviewDetails({ navigation }) {
-    const pressHandler = () => {
-        navigation.goBack();
-        // navigation.pop();
-    }
-
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>ReviewDetails Screen</Text>
-            <Button title="Back to home screen" onPress={pressHandler} />
+            <Text>{ navigation.getParam('title') }</Text>
+            <Text>{ navigation.getParam('body') }</Text>
+            <Text>{ navigation.getParam('rating') }</Text>
         </View>
     )
 }
