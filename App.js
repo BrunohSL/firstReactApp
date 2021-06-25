f4daf6f2214c726c8131121f542d67a48840e5b1
Bruno Heinrich <brunu1994@gmail.com>
2021-06-25 12:16:26 -0300

    Using AppLoading to load the custom fonts

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font'
 import Home from './screens/home';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require("./assets/Fonts/Nunito-Regular.ttf"),
+  'nunito-bold': require("./assets/Fonts/Nunito-Bold.ttf"),
+});
 
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.log("Error")}
+      />
+    );
+  }
 }
