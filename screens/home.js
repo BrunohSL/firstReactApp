0a84d124b7fc753a288dd17322fb26becaaac15f
Bruno Heinrich <brunu1994@gmail.com>
2021-06-29 13:48:25 -0300

    creating form

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList, Modal } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
+import { MaterialIcons } from '@expo/vector-icons';
+import ReviewForm from './reviewForm';
 
 export default function Home({ navigation }) {
+    const [modalOpen, setModalOpen] = useState(false);
+
     const [reviews, setReviews] = useState([
         { title: 'Zelda', rating: 5, body: 'lorem ipsum', key: 1},
         { title: 'Pokemon', rating: 4, body: 'lorem ipsum', key: 2},
@@ -12,6 +16,25 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
+            <Modal visible ={modalOpen} animationType={'slide'}>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        style={{ ...styles.modalToggle, ...styles.modalClose}}
+                        onPress={() => setModalOpen(false)}
+                    />
+                    <ReviewForm />
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
+            />
+
             <FlatList
                 data={reviews}
                 renderItem={({ item }) => (
@@ -25,3 +48,21 @@ export default function Home({ navigation }) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+    },
+    modalContent: {
+        flex: 1,
+    }
+})
